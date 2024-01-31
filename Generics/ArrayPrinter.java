ff94c68a67ae66b2a06f6e4006349b3a7948438e
jamessciacca <noing2003@gmail.com>
2024-01-31 09:17:26 -0500

    added notes

--- Generics/ArrayPrinter.java
+++ Generics/ArrayPrinter.java
@@ -26,3 +26,12 @@ public class ArrayPrinter{
         System.out.println();
     }
 }
+
+//Line 21: public static <T> void printArray(T[] array){ This line declares a public static method named printArray. The <T> denotes that this method uses generics, meaning it can work with any object type. The method takes one parameter: an array of any type T.
+
+//Line 22: for(T element : array){ This line starts a for-each loop that iterates over each element in the input array. The type of element is T, which will match whatever type of objects are in the array.
+
+//Line 23: System.out.printf("%s ", element); This line prints the current element to the console. The %s is a placeholder that gets replaced by the string representation of element.
+
+//Line 25: System.out.println(); This line prints a newline to the console, which helps separate the output of different calls to printArray.
+    
